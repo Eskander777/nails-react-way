9958b941f7d478cc84d0116b1cd5275572485830
Aleksandr <aleksandros-777@mail.ru>
2020-06-06 08:48:26 +0300

    changes in responsive design

--- src/components/Navigation/Sidedrawer/Sidedrawer.js
+++ src/components/Navigation/Sidedrawer/Sidedrawer.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 
 import './sidedrawer.css';
 
 const Sidedrawer = (props) => {
-  let sideDrawerClassName = props.sideShowState
-    ? 'mobile-nav open'
-    : 'mobile-nav';
+  let sideDrawerBoolean = props.sideShowState ? true : false;
 
   return (
-    <nav className={sideDrawerClassName}>
-      <ul className="mobile-nav__items">
-        <li className="mobile-nav__item">
-          <Link to="/" onClick={props.close}>
-            Главная
-          </Link>
-        </li>
-        <li className="mobile-nav__item">
-          <Link to="../exapmles" onClick={props.close}>
-            Примеры работ
-          </Link>
-        </li>
-        <li className="mobile-nav__item">
-          <Link to="../order_page" onClick={props.close}>
-            Записаться сейчас
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <CSSTransition
+      mountOnEnter
+      unmountOnExit
+      in={sideDrawerBoolean}
+      timeout={200}
+      classNames='open-sidedrawer'
+    >
+      <nav className="mobile-nav">
+        <ul className="mobile-nav__items">
+          <li className="mobile-nav__item">
+            <Link to="/" onClick={props.close}>
+              Главная
+            </Link>
+          </li>
+          <li className="mobile-nav__item">
+            <Link to="../exapmles" onClick={props.close}>
+              Примеры работ
+            </Link>
+          </li>
+          <li className="mobile-nav__item">
+            <Link to="../order_page" onClick={props.close}>
+              Записаться сейчас
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </CSSTransition>
   );
 };
 
